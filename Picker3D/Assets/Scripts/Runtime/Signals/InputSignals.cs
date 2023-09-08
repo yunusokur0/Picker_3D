c49d79460a08ff9b94aab1becc82e615be219f68
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-09 01:46:29 +0300

    Update InputSignals.cs

--- Picker3D/Assets/Scripts/Runtime/Signals/InputSignals.cs
+++ Picker3D/Assets/Scripts/Runtime/Signals/InputSignals.cs
@@ -1,20 +1,4 @@
 using UnityEngine.Events;
-
-//public class InputSignals : MonoBehaviour
-//{
-//public static InputSignals Instance;
-////*****************************************
-//public UnityAction onFirstTimeTouchTaken = delegate { };
-////Alýnan Giriþte, hareket edebilirlik
-//public UnityAction onInputTaken = delegate { };
-////giris serbest birakildiginda
-//public UnityAction onInputReleased = delegate { };
-////giris etkinlestirirlir
-//public UnityAction onEnableInput = delegate { };
-////giris devradisi birakilir 
-//public UnityAction onDisableInput = delegate { };
-
-//public UnityAction<HorizontalInputParams> onInputDragged = delegate { };
 public class InputSignals : MonoSingleton<InputSignals>
 {
     public UnityAction onEnableInput = delegate { };
